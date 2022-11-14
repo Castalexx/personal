9d5c03d4560fb3cf2d6d07458b7d899da11190df
Castalexx <90343321+Castalexx@users.noreply.github.com>
2022-11-14 13:04:09 -0300

    Primera subida

---
+++ js.js
@@ -0,0 +1,14 @@
+/* 
+Hacer un loop rebote para el video
+ */
+id = setInterval(frame, 5);
+
+function frame() {
+  if (el video llega a su fin) {
+    clearInterval(id);
+  } else {
+    /* code to change the element style */  empezar desde atras}
+}
+
+/* Hacer animaciones para los iconos */
+
